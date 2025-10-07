e9ca3998547a7e8f86caa7da7e2fc5ba08817326
Nader55555 <101871112+Nader55555@users.noreply.github.com>
2025-10-07 21:43:13 +0300

    Update pythondemography.py

--- pythondemography.py
+++ pythondemography.py
@@ -171,14 +171,19 @@ st.plotly_chart(fig, use_container_width=True)
 #     "regions with higher or lower youth populations.</div>",
 #     unsafe_allow_html=True
 # )
-st.markdown(
-    "<div class='note'>The box plot shows the percentage of youth (15–24 years old) in different regions of Lebanon. "
-    "It helps compare which regions have more young people and how spread out their numbers are.  \n\n"
-    "**Insights:**  \n"
-    "• **Tripoli** and **Baalbek-Hermel** have some of the highest youth percentages, showing that these areas have a larger young population compared to others.  \n"
-    "• Regions like **Mount Lebanon** and **Beirut** have more mixed values, meaning some parts have many young people while others have fewer.</div>"
-)
 
+st.markdown(
+    """
+    <div class='note'>
+    The box plot shows the percentage of youth (15–24 years old) in different regions of Lebanon.
+    It helps compare which regions have more young people and how spread out their numbers are. <br><br>
+    <b>Insights:</b><br>
+    • <b>Tripoli</b> and <b>Baalbek-Hermel</b> have some of the highest youth percentages, showing that these areas have a larger young population compared to others. <br>
+    • Regions like <b>Mount Lebanon</b> and <b>Beirut</b> have more mixed values, meaning some parts have many young people while others have fewer.
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 
 st.divider()
@@ -204,6 +209,7 @@ st.markdown(
     "It helps identify regions with higher or lower elderly populations.</div>",
     unsafe_allow_html=True
 )
+
 
 
 
