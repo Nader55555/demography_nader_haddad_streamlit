b462b2159e0850daf5ddb6401d3b586e57cf5bc5
Nader55555 <101871112+Nader55555@users.noreply.github.com>
2025-10-08 17:55:14 +0300

    Update pythondemography.py

--- pythondemography.py
+++ pythondemography.py
@@ -226,9 +226,9 @@ selected_regions = st.multiselect(
 )
 
 # Family size radio
- st.markdown("<div class='label-box'>Family Size View</div>", unsafe_allow_html=True)
+ 
 size_choice = st.radio(
-    
+    "Family Size View"
     ["All", "1-3", "4-6", "7+"],
     index=0,
     horizontal=True
@@ -425,6 +425,7 @@ st.markdown(
     "It helps identify regions with higher or lower elderly populations.</div>",
     unsafe_allow_html=True
 )
+
 
 
 
